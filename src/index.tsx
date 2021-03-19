b8fc8a30a113621adc31547750878368b1c48e03
adriangvaldes <adriangvaldes@gmail.com>
2021-03-19 18:57:52 -0300

    Finalizing app

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
-createServer({
-  routes() {
-    this.namespace = 'api';
+createServer({      // Mirage 'fake server' creation and functionalities
+  models: {
+    transaction: Model,
+  },
 
-    this.get('/transactions', () => {
-      return [
+  seeds(server) {   //Definindo valores iniciais do banco
+    server.db.loadData({
+      transactions: [
         {
           id: 1,
-          title: 'Transaction 1',
-          amount: 400,
+          title: 'Freelancer de website',
           type: 'deposit',
-          category: 'Food',
-          cretedAt: new Date()
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Alugel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-02-14 11:00:00'),
         }
       ]
+    })
+  },
+
+  routes() {
+    this.namespace = 'api';    //Definindo o caminho padrão api/
+
+    this.get('/transactions', () => {       //Metodo de pegar info do banco usando get
+      return this.schema.all('transaction')    //Retorna tudo q tiver em transactions
+    })
+
+    this.post('/transactions', (schema, request) => {   //Posta tudo que tiver no request e guarda na variavel via parse para transformar em JSON
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)  //Coloca no banco 'schema' uma nova celula na parte de transaction, usando os dados de 'data'
     })
   }
 })
